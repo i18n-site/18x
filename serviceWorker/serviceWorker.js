e12737d63e2953d2a0c998855632a4991b980284
i18n <i18n.site@gmail.com>
2024-05-31 17:21:24 +0800

    ♨

--- serviceWorker/serviceWorker.js
+++ serviceWorker/serviceWorker.js
@@ -153,8 +153,6 @@ const S = (cdn, proxy_li) => {
 							console.error(furl, err)
 						}
 						li.push(li.shift())
-						// 失败了 成功次数减半
-						info[0] = Math.round(info[0] / 2)
 						save(1)
 					}
 				}

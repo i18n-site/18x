1b1c2c401a506dfc55277e8888fb9aa56ce70ddc
js0.site <jssite@googlegroups.com>
2026-03-10 16:40:41 +0800

    .

--- src/i-h.js
+++ src/i-h.js
@@ -1,17 +1,17 @@
-import onI18n from "x/langSite.js"
+import onI18n from "x/langSite.js";
 
 customElements.define(
-	"i-h",
-	class extends HTMLElement {
-		connectedCallback() {
-			const me = this,
-				f = _H[me.dataset.h]
-			if (f) {
-				me._ = onI18n(() => (me.innerHTML = f(_L)))
-			}
-		}
-		disconnectedCallback() {
-			this._ && this._()
-		}
-	},
-)
+  "i-h",
+  class extends HTMLElement {
+    connectedCallback() {
+      const me = this,
+        f = _H[me.dataset.h];
+      if (f) {
+        me._ = onI18n(() => (me.innerHTML = f(_L)));
+      }
+    }
+    disconnectedCallback() {
+      this._ && this._();
+    }
+  },
+);
